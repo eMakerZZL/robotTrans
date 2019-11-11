da11fcba43a3075b15ee3b366ddeefb04c7e6f22
ZhangZL <ZhangZL@123.com>
2019-11-11 09:03:51 +0800

    delete useless file and repair solve_cart_euler.m

--- solve_cart_euler.m
+++ solve_cart_euler.m
@@ -1,8 +1,8 @@
-function output_mat = solve_cart_euler(intput_mat)
+function output_mat = solve_cart_euler(input_mat)
     n = 1;x = 1;
     o = 2;y = 2;
     a = 3;z = 3;
-    mat = @(aixs, part) intput_mat(part,axis);
+    mat = input_mat(1:3,1:3).';
 
     theta_a0(1,1) = atan2( mat(a,y), mat(a,x));
     theta_a0(2,1) = atan2(-mat(a,y),-mat(a,x));
@@ -14,5 +14,5 @@ function output_mat = solve_cart_euler(intput_mat)
     theta_o(2,1) = atan2(mat(a,x) * cos(theta_a0(2,1)) + mat(a,y) * sin(theta_a0(2,1)), mat(a,z));
 
     output_mat = [theta_a0'; theta_o'; theta_a1'];
-    output_mat = [output_mat,intput_mat(1:3,:)];
+    output_mat = [output_mat,input_mat(1:3,4)];
 end
